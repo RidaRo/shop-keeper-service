df1235e814804a068db5236fc88d18e791d11a81
Roman Rida <Roman_Rida@epam.com>
2022-01-11 23:16:32 +0300

    Initial commit

---
+++ src/test/java/com/example/training/shop/keeper/ShopKeeperApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.training.shop.keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+@SpringBootTest
+class ShopKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assert.isTrue(true);
+	}
+
+}
