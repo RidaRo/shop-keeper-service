544b9912c6349dc9dbc14888caa9a242d9a0ef14
Roman Rida <Roman_Rida@epam.com>
2022-02-08 22:54:06 +0300

    Changed application.properties to more suit production. deploy.yml and DeployCDK.yml changed accordingly.
    ShopKeeperInfrastructureStack changed to deploy RDS more successfully

--- shop-keeper-infrastructure/src/main/java/com/myorg/ShopKeeperInfrastructureStack.java
+++ shop-keeper-infrastructure/src/main/java/com/myorg/ShopKeeperInfrastructureStack.java
@@ -7,11 +7,14 @@ import software.amazon.awscdk.services.ec2.*;
 import software.amazon.awscdk.services.ecr.Repository;
 import software.amazon.awscdk.services.ecs.Cluster;
 import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.Secret;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.secretsmanager.ISecret;
 import software.constructs.Construct;
+
+import java.util.Map;
 
 public class ShopKeeperInfrastructureStack extends Stack {
     private final ISecret postgresSecret;
@@ -33,16 +36,38 @@ public class ShopKeeperInfrastructureStack extends Stack {
                 .defaultValue("keeper")
                 .build();
 
+        var imageTag = CfnParameter.Builder.create(this, "imageTag")
+                .type("String")
+                .description("The tag of ecr container")
+                .build();
+
         postgresSecret = DatabaseSecret.Builder.create(this, "PostgresCredentials")
                 .username(postgresUsername.getValueAsString())
                 .build();
 
         Vpc vpc = Vpc.Builder.create(this, "MyVpc")
-                .maxAzs(3)  // Default is all AZs in region
+                .maxAzs(2)  // Default is all AZs in region
+                .subnetConfiguration(Vpc.DEFAULT_SUBNETS_NO_NAT)
                 .build();
 
         Cluster cluster = Cluster.Builder.create(this, "MyCluster")
                 .vpc(vpc).build();
+
+        var database = DatabaseInstance.Builder.create(this, "Postgres")
+                .engine(DatabaseInstanceEngine.postgres(
+                                PostgresInstanceEngineProps.builder()
+                                        .version(PostgresEngineVersion.VER_13_4)
+                                        .build()
+                        )
+                )
+                .vpc(vpc)
+                .vpcSubnets(SubnetSelection.builder().subnetType(SubnetType.PRIVATE_ISOLATED).build())
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO))
+                .databaseName(postgresDatabaseName.getValueAsString())
+                .credentials(Credentials.fromSecret(postgresSecret))
+                .build();
+
+        database.getConnections().allowFromAnyIpv4(Port.tcp(5432), "Allow connections to the database");
 
         // Create a load-balanced Fargate service and make it public
         ApplicationLoadBalancedFargateService.Builder.create(this, "MyFargateService")
@@ -54,28 +79,19 @@ public class ShopKeeperInfrastructureStack extends Stack {
                                 .image(ContainerImage
                                         .fromEcrRepository(Repository
                                                         .fromRepositoryName(this, "EcrRepository", "shop-keeper-service"),
-                                                "a45fe36c236779e5eb12b5391e11d1b836dbfc95"))
+                                                imageTag.getValueAsString()))
                                 .containerPort(8080)
+                                .environment(Map.of
+                                        ("POSTGRES_HOST", database.getDbInstanceEndpointAddress(),
+                                         "POSTGRES_PORT", database.getConnections().getDefaultPort().toString(),
+                                         "POSTGRES_DATABASE", postgresDatabaseName.getValueAsString()))
+                                .secrets(Map.of
+                                        ("POSTGRES_PASSWORD", Secret.fromSecretsManager(postgresSecret, "password"),
+                                         "POSTGRES_NAME", Secret.fromSecretsManager(postgresSecret, "username")))
                                 .build())
                 .memoryLimitMiB(1024)       // Default is 512
                 .publicLoadBalancer(true)   // Default is false
                 .build();
 
-        var database = DatabaseInstance.Builder.create(this, "Postgres")
-                .engine(DatabaseInstanceEngine.postgres(
-                                PostgresInstanceEngineProps.builder()
-                                        .version(PostgresEngineVersion.VER_13_4)
-                                        .build()
-                        )
-                )
-                .vpc(vpc)
-                //todo ask about error when choosing SubnetType.PRIVATE_ISOLATED
-                .vpcSubnets(SubnetSelection.builder().subnetType(SubnetType.PUBLIC).build())
-                .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO))
-                .databaseName(postgresDatabaseName.getValueAsString())
-                .credentials(Credentials.fromSecret(postgresSecret))
-                .build();
-
-        database.getConnections().allowFromAnyIpv4(Port.tcp(5432), "Allow connections to the database");
     }
 }
