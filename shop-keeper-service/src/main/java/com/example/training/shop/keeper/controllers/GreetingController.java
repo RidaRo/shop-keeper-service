b681423a6697880b74860bbb3a81764e15a49e0e
Roman Rida <Roman_Rida@epam.com>
2022-04-06 13:53:01 +0300

    Initial configuration for oauth security

--- shop-keeper-service/src/main/java/com/example/training/shop/keeper/controllers/GreetingController.java
+++ shop-keeper-service/src/main/java/com/example/training/shop/keeper/controllers/GreetingController.java
@@ -1,13 +1,16 @@
 package com.example.training.shop.keeper.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.annotation.security.RolesAllowed;
+
+@RestController
 public class GreetingController {
 
     @GetMapping("/")
+    @RolesAllowed("items_read")
     public @ResponseBody
     String greeting() {
         return "Hello, World";
